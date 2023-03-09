460c1741eda9ecdcd65607ff78192d8dfb627562
roberto <robertoasanchez95@gmail.com>
2023-03-09 10:41:53 -0300

    actualizacion que funciones y envio de data a google sheets

--- leecsv.py
+++ leecsv.py
@@ -1,8 +1,12 @@
+import re
 import pandas as pd
 import json
-import ast
 from datetime import datetime
+from sendData import exportar
 
+dfDescription = pd.DataFrame(columns=['Description'])
+dfData = pd.DataFrame(columns=['Valor', 'Total', 'Porcentaje'],
+                    data=[['Valor', 'Total','porcentaje']])
 
 def monitor():
     '''Imprime 20 filas del merge'''
@@ -10,7 +14,7 @@ def monitor():
     for x in list(df.columns):
         print(df[x].head(20))
 
-def merge() :
+def merge():
     '''Une la data de Fire y History por el ID del incendio, lo guarda como CSV'''
     fire = pd.read_csv("fires.csv", sep="_")
     history = pd.read_csv("history.csv", sep="_")
@@ -19,28 +23,45 @@ def merge() :
     print(dfMerge)
 
 def firesNotInHistory():
-    '''Devuelve los datos que no posean registro. Y la cantidad de estos'''
+    '''Devuelve los datos que no posean registro.'''
     df = pd.read_csv("data/merge.csv", sep="_")
+    total = len(df)
     df = df[df['_merge'] == 'left_only']
     casosPresentados = df.shape[0]
-    return df, casosPresentados
+    porcentaje = (casosPresentados/total)*100
+    string = '{:.2f}%'.format(porcentaje)
+    dfData.loc[len(dfData)] = [casosPresentados, total, string]
+    dfDescription.loc[len(dfDescription)] = ["Fuegos sin Reporte"]
+    return df
 
 def historyNotInFires():
-    '''Devuelve los datos que no poseen incendios. Y su cantidad.'''
+    '''Devuelve los datos que no poseen incendios.'''
     df = pd.read_csv("data/merge.csv", sep="_")
+    total = len(df)
     df = df[df['_merge'] == 'right_only']
     casosPresentados = df.shape[0]
-    return df, casosPresentados
+    porcentaje = (casosPresentados/total)*100
+    string = '{:.2f}%'.format(porcentaje)
+    dfData.loc[len(dfData)] = [casosPresentados, total, string]
+    dfDescription.loc[len(dfDescription)] = ["Reportes sin fuegos"]
+    return df
 
 def fireInHistory():
-    '''Devuelve los incendios que si poseen reporte, y su cantidad.'''
+    '''Devuelve los incendios que si poseen reporte.'''
     df = pd.read_csv("data/merge.csv", sep="_")
+    total = len(df)
     df = df[df['_merge'] == "both"]
     casosPresentados = df.shape[0]
-    return df, casosPresentados
+    porcentaje = (casosPresentados/total)*100
+    string = '{:.2f}%'.format(porcentaje)
+    dfData.loc[len(dfData)] = [casosPresentados, total, string]
+    dfDescription.loc[len(dfDescription)] = ["Fuegos con Reporte"]
+    return df 
 
 def reportBank(firesInHistory: pd.DataFrame, firesNotInHistory: pd.DataFrame):
     '''Entrega la información de los incendios y reportes agrupados por banco'''
+    listData = []
+    
     withReport = firesInHistory
     noReport = firesNotInHistory
     
@@ -52,53 +73,72 @@ def reportBank(firesInHistory: pd.DataFrame, firesNotInHistory: pd.DataFrame):
     diccionarioNoReport = {i: json.loads(x) for i, x in enumerate(systemListNoReport)}
     dfNombresNoReport = pd.DataFrame.from_dict(diccionarioNoReport, orient="index")
     
-    countsIn = dfNombres['system_name'].value_counts()
-    countsNotIn = dfNombresNoReport['system_name'].value_counts()
+    countsIn = dfNombres.groupby('system_name').count().drop(columns=['version', 
+                                                                    'utc_minute_offset',
+                                                                    'utc_offset']).rename(columns={'api_key': 'Reportados'})
+    countsNotIn = dfNombresNoReport.groupby('system_name').count().drop(columns=['version', 
+                                                                                'utc_minute_offset',
+                                                                                'utc_offset']).rename(columns={'api_key': 'No Reportados'})
     
-    print(f"Lista de incendios con historial por cada banco:\n{countsIn}\n")
-    print(f"Lista de incendios sin historial por cada banco:\n{countsNotIn}\n")
+    df = pd.merge(countsIn, countsNotIn, left_on='system_name', right_on='system_name', how='outer')    
+    #elimina los decimales de los valores
+    df = df.fillna(0).astype(int)
+    dfText = pd.DataFrame(df.index)
+    nuevo_dato = pd.DataFrame({'Reportados': ['Reportados'], 'No Reportados': ['No Reportados']})
+    df = pd.concat([nuevo_dato,df], ignore_index=True)
+    exportar(dfText, df, 2, date)
+
+def msg(string):
+    result = re.findall(r"(?:(?<!\{)\b.*?(?='?\},|$))", string)
+    print(string)
+    print(result)
+    #'msg': ''
 
 def msjPromedio(firesInHistory: pd.DataFrame):
     '''Promedia el numero de mensajes por reporte.'''
     numeroMsg = []
     df = firesInHistory
-    for element in df["events"]:
+    #df["events"].head(1).apply(lambda x: msg(x))
+    for element in df["events"].head(10):
         count = element.count("state")
         numeroMsg.append(count)
     promedio = sum(numeroMsg)/len(numeroMsg)
-    print(f"El promedio de mensajes por reporte es de {promedio:.2f}\n")
+    dfDescription.loc[len(dfDescription)] = ["Mensajes promedio por reporte"]
+    dfData.loc[len(dfData)] = [promedio, None, None]
 
 # Funcion para extraer los numeros de la variable "time" usando regex.
 # Ej:
 # {'system_detection': False, 'ti2023, 2, 1, 0, 41, 'time': DatetimeWithNanoseconds(2023, 2, 1, 0, 41, 48, tzinfo=datetime.timezone.utc), 'log_time': DatetimeWithNanoseconds(2023, 2, 1, 0, 42, 4, tzinfo=datetime.timezone.utc), 'online_time': DatetimeWithNanoseconds(2023, 2, 1, 0, 42, 11, 887204, tzinfo=datetime.timezone.utc), 'atime': DatetimeWithNanoseconds(2023, 2, 1, 0, 40, 8, tzinfo=datetime.timezone.utc)}
 def time(x):
-    import re
     results = re.findall(r"'time': DatetimeWithNanoseconds\((.*?), tzinfo=datetime.timezone.utc\)", x)
     # results = [datetime.strptime(x, '%Y, %m, %d, %H, %M, %S') for x in results]
-    print(results[0])
-    return results
-
-
-def funcion(x:str)->datetime:
-    print(x)
-    data = ''
-    return data
+    fecha = results[0]
+    try:
+        # Intenta convertir la fecha con segundos
+        fechaConvertida = datetime.strptime(fecha, '%Y, %m, %d, %H, %M, %S')
+    except ValueError:
+        # Si falla, intenta convertir la fecha sin segundos
+        fechaConvertida = datetime.strptime(fecha, '%Y, %m, %d, %H, %M')
+    return fechaConvertida
 
 def promReactionOpen(firesInHistory: pd.DataFrame):
     """promedia el tiempo de reacción entre detección y apertura """
     df = firesInHistory
-    datetime(2023, 2, 1, 0, 41, 48)
-    
-    df["times"].apply(lambda x: time (x))
-    #diccionario = df["times"].to_dict()
-    #df['times'] = df['times'].apply(lambda x: ast.literal_eval(x))
-    #df['times'] = df['times'].apply(lambda x: pd.Series(json.loads(x)).to_dict())
-    #systemList = [x.replace("'", "\"") for x in df["times"].tolist()]
-    #diccionario = json.dumps(systemList)
-    #dfTimes = pd.read_json(diccionario)
-    #df["times"].to_csv("time.csv",sep="_") 
-    #print(diccionario.keys())
-    #print(df["times"].to_dict())
+    promTimes = []
+    for index, row in df.iterrows():
+        dateFire = time (row["times"])
+        openTime = pd.to_datetime(row['timestamp_open']).to_pydatetime().replace(tzinfo=None)
+        times = openTime - dateFire
+        promTimes.append(times)
+    df.insert(len(df.columns), "reaction_time", promTimes)
+    promTimes = pd.Series(promTimes)
+    promedio = promTimes.mean()
+    horas, rem = divmod (promedio.total_seconds(), 3600)
+    minutos, segundos = divmod(rem, 60)
+    time_str = '{:02.0f}:{:02.0f}:{:02.0f}'.format(horas, minutos, segundos)
+    df['reaction_time'] = df['reaction_time'].astype(str)
+    dfData.loc[len(dfData)] = [time_str, None, None]
+    dfDescription.loc[len(dfDescription)] = ["Tiempo promedio de reacción a incendios"]
 
 def promOpenClose(firesInHistory: pd.DataFrame):
     '''Promedia el tiempo entre que se abre y cierra un reporte'''
@@ -114,58 +154,73 @@ def promOpenClose(firesInHistory: pd.DataFrame):
     horas, rem = divmod (promedio.total_seconds(), 3600)
     minutos, segundos = divmod(rem, 60)
     time_str = '{:02.0f}:{:02.0f}:{:02.0f}'.format(horas, minutos, segundos)
-
-    print(f"El tiempo promedio entre apertura y cierre es de {time_str}\n")
+    dfData.loc[len(dfData)] = [time_str, None, None]
+    dfDescription.loc[len(dfDescription)] = ["Tiempo promedio apertura y cierre"]
 
 def reportDispatcher():
     '''Muestra los reportes que han sido despachados (JSON Uruguay)'''
-    resultados = []
     df = pd.read_csv("data/merge.csv", sep="_")
-    resultados.append(df["json_sended"].value_counts(dropna=False))
-    for diccionario in resultados:
-        for clave, valor in diccionario.items():
-            if clave == True:
-                print(f"Reportes de despacho: {valor}.\n")
-            elif clave == False:
-                print(f"Reportes que no son de despacho: {valor}.")
-            else :
-                print(f"Reportes sin información: {valor}.")
-    #print (df.json_sended)
+    dfUruguay = df.loc[(df["client_x"] == "SPF") & (df["json_sended"].notnull())]
+    dfDescription.loc[len(dfDescription)] = ["Reportes despachados para Uruguay"]
+    dfData.loc[len(dfData)] = [len(dfUruguay), None, None]
 
 def reportForOperator():
     '''Reportes agrupados por operador.'''
     df = pd.read_csv("data/merge.csv", sep="_")
-    lista = df.groupby("operator").count()
-    print(f"Lista de reportes por operador\n{lista.fire_x}\n")
+    newDataFrame = df.groupby("operator").count()
+    newDataFrame = newDataFrame[['fire_x']]
+    newDataFrame.rename(columns={"fire_x": "N° Reportes"}, inplace=True)
+    dfText = pd.DataFrame(newDataFrame.index)
+    newData = pd.DataFrame({'N° Reportes': ['N° Reportes']})
+    newDataFrame = pd.concat([newData, newDataFrame], ignore_index=True)
+    exportar(dfText, newDataFrame, 3, date) 
 
 def reportNotInternalId():
     '''Numero de reportes sin ID interno'''
     df = pd.read_csv("data/merge.csv", sep="_")
+    total = len(df)
     count = df["internal_id"].isna().sum()
-    #print(df["internal_id"].value_counts(dropna=False))
-    print(f"la cantidad de reportes sin Id interno es de {count}\n")
+    porcentaje = (count/total)*100
+    string = '{:.2f}%'.format(porcentaje)
+    dfData.loc[len(dfData)] = [count, total, string]
+    dfDescription.loc[len(dfDescription)] = ["Reportes sin Id interno"]
 
 def poolForDispatcher(firesInHistory : pd.DataFrame):
     '''Numero de reportes por despachador.'''
     df = firesInHistory
-    agrupado = df.groupby("dispatcher").count()
-    print(f"Lista de historial por despachador\n{agrupado.id}\n")
+    DataFrameDispachers = df.groupby("dispatcher").count()
+    DataFrameDispachers = DataFrameDispachers[['id']]
+    DataFrameDispachers.rename(columns={"id": "N° Reportes"}, inplace=True)
+    dfText = pd.DataFrame(DataFrameDispachers.index)
+    newData = pd.DataFrame({'N° Reportes': ['N° Reportes']})
+    DataFrameDispachers = pd.concat([newData, DataFrameDispachers], ignore_index=True)
+    exportar(dfText, DataFrameDispachers, 4, date) 
 
+def takeTime():
+    '''Toma la hora de inicio y final de la data'''
+    df = pd.read_csv("data/merge.csv", sep="_")
+    open = pd.to_datetime(df["timestamp_open"])
+    close = pd.to_datetime(df["timestamp_close"])
+    open = open.min().date().strftime("%d/%m")
+    close = close.max().date().strftime("%d/%m")
+    date = "Periodos: " + open + " - " + close
+    return (date)
 
 #monitor()
-dfFireInHistory, countFireInHistory = fireInHistory()
-dfFireNotInHistory, countFireNotInHistory = firesNotInHistory()
-dfHistoryNotInFire, countHistoryNotInFire = historyNotInFires()
+date = takeTime()
 
-# print (f"La cantidad de fuegos con historial es de {countFireInHistory}.")
-# print(f"La cantidad de fuegos sin historial es de {countFireNotInHistory}.")
-# print(f"La cantidad de Historial sin fuegos es de {countHistoryNotInFire}.\n")
-# reportNotInternalId()
+dfFireInHistory = fireInHistory()
+dfFireNotInHistory = firesNotInHistory()
+dfHistoryNotInFire = historyNotInFires()
+
+reportNotInternalId()
 promReactionOpen(dfFireInHistory)
-# promOpenClose(dfFireInHistory)
-#reportDispatcher()
-# msjPromedio(dfFireInHistory)
+promOpenClose(dfFireInHistory)
+reportDispatcher()
+msjPromedio(dfFireInHistory)
 
-# reportBank(dfFireInHistory, dfFireNotInHistory)
-# reportForOperator()
-# poolForDispatcher(dfFireInHistory)
+reportBank(dfFireInHistory, dfFireNotInHistory)
+reportForOperator()
+poolForDispatcher(dfFireInHistory)
+
+exportar(dfDescription, dfData, 1, date)
--- sendData.py
+++ sendData.py
@@ -1,4 +1,5 @@
 import gspread
+import gspread.utils as gsutils
 import pandas as pd
 
 def credenciales ():
@@ -8,7 +9,121 @@ def credenciales ():
     Returns:
         hoja_excel: URL de documento google sheets 
     """
-    gc = gspread.service_account(filename='C:\proyectos\sos\credenciales\credentials.json')
+    gc = gspread.service_account(filename='C:\proyectos\querysOperators\credenciales\credentials.json')
     hoja_excel = gc.open_by_url('https://docs.google.com/spreadsheets/d/1vhKqsudLV-iyIbL8GReUOqi02tj16LUZmvGc3o0C5lE/edit#gid=0')
     return hoja_excel
 
+def consultar_id (hoja:int):
+    """
+    ## Descripción:
+    Extrae ID de fichas registradas en google sheets
+    ### Args:
+        hoja (int): Index de hoja de donde extraer IDs
+
+    ### Returns:
+        ids: Lista de IDs ya registrados en google sheets
+    """
+    hoja_excel = credenciales()
+    hoja1 = hoja_excel.get_worksheet(hoja)
+    ids = hoja1.col_values(1) #extrae columna de ID
+    if len(ids) != 0:
+        ids.pop(0) # elimina la etiqueta de la lista
+    return ids
+
+def consultar_data(hoja:int, inicio, fin):
+    """Consulta si existe la primera fila de la hoja de trabajo"""
+    hoja_excel = credenciales()
+    pagina = hoja_excel.get_worksheet(hoja)
+    data= pagina.range(f'{inicio}:{fin}')
+    flag = True
+    for cell in data:
+        if cell.value != '':
+            flag = False
+    return flag
+
+def sendPage1(pagina:gspread.worksheet, hoja:int, dftext:pd.DataFrame, dfData:pd.DataFrame, date:str):
+    description = consultar_data(hoja, 'A3', 'A10')
+    dfData.fillna('', inplace=True)
+    if description:
+        pagina.merge_cells('B1:D1')
+        #fuciona celdas de google sheets B1,C1 y D1
+        pagina.update('B1:D1', date)
+        #ingresa dftext en columna A desde la fila 3
+        pagina.update('A3', dftext.values.tolist())
+        pagina.update('B2', dfData.astype(str).values.tolist())
+    else:
+        col = len(pagina.row_values(3)) + 1
+        dateInit = gsutils.rowcol_to_a1(1, col)
+        dateEnd = gsutils.rowcol_to_a1(1, col+2)
+        pagina.merge_cells(f'{dateInit}:{dateEnd}')
+        # incerta fecha en celdas B1, C1 y D1 centrando el texto
+        pagina.update(f'{dateInit}:{dateEnd}', date)
+        #selecciona la fila de google sheets en base a row
+        rowCategory = gsutils.rowcol_to_a1(2, col)
+        #ingresa nombre de categorias en la fila 2 de dfdata   
+        pagina.update(rowCategory , dfData.astype(str).values.tolist())
+
+def sendPage2(pagina, hoja, dfText, dfData, date):
+    description = consultar_data(hoja, 'A3','A14')
+    if description:
+        pagina.merge_cells('B1:C1')
+        pagina.update('B1:C1', date)
+        #ingresa dftext en columna A desde la fila 3
+        pagina.update('A3', dfText.values.tolist())
+        pagina.update('B2', dfData.astype(str).values.tolist())
+    else:
+        col = len(pagina.row_values(3)) + 1
+        dateInit = gsutils.rowcol_to_a1(1, col)
+        dateEnd = gsutils.rowcol_to_a1(1, col+1)
+        # incerta fecha en celdas B1, C1 y D1 centrando el texto
+        pagina.merge_cells(f'{dateInit}:{dateEnd}')
+        pagina.update(f'{dateInit}:{dateEnd}', date)
+        #selecciona la fila de google sheets en base a row
+        rowCategory = gsutils.rowcol_to_a1(2, col)
+        #ingresa nombre de categorias en la fila 2 de dfdata   
+        pagina.update(rowCategory , dfData.astype(str).values.tolist())
+
+def sendPage3(pagina, hoja, dftext, dfData, date):
+    description = consultar_data(hoja, 'A3','A52')
+    if description:
+        pagina.update('B1', date)
+        #ingresa dftext en columna A desde la fila 3
+        pagina.update('A3', dftext.values.tolist())
+        pagina.update('B2', dfData.astype(str).values.tolist())
+    else:
+        row = len(pagina.row_values(3)) + 1
+        #selecciona la fila de google sheets en base a row
+        rowCategory = gsutils.rowcol_to_a1(2, row)
+        rowDate = gsutils.rowcol_to_a1(1, row)
+        pagina.update(rowDate, date)
+        #ingresa nombre de categorias en la fila 2 de dfdata   
+        pagina.update(rowCategory , dfData.astype(str).values.tolist())
+
+def sendPage4(pagina, hoja, dftext, dfData, date):
+    description = consultar_data(hoja, 'A3','A52')
+    if description:
+        pagina.update('B1', date)
+        #ingresa dftext en columna A desde la fila 3
+        pagina.update('A3', dftext.values.tolist())
+        pagina.update('B2', dfData.astype(str).values.tolist())
+    else:
+        row = len(pagina.row_values(3)) + 1
+        #selecciona la fila de google sheets en base a row
+        rowCategory = gsutils.rowcol_to_a1(2, row)
+        rowDate = gsutils.rowcol_to_a1(1, row)
+        pagina.update(rowDate, date)
+        #ingresa nombre de categorias en la fila 2 de dfdata   
+        pagina.update(rowCategory , dfData.astype(str).values.tolist())
+
+def exportar(dftext:pd.DataFrame, dfData:pd.DataFrame, hoja:int, date:str):
+    """recive un dataframe y clasifica la información para enviarla a la hoja de trabajo"""
+    hoja_excel = credenciales()
+    pagina = hoja_excel.get_worksheet(hoja)#selecciona hoja de trabajo
+    if (hoja == 1):
+        sendPage1(pagina, hoja, dftext, dfData, date)
+    elif(hoja == 2):
+        sendPage2(pagina, hoja, dftext, dfData , date)
+    elif(hoja == 3):
+        sendPage3(pagina, hoja, dftext, dfData, date)
+    elif(hoja == 4):
+        sendPage4(pagina, hoja, dftext, dfData, date)
